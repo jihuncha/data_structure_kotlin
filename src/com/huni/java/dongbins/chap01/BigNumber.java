c1349d7dec9f088b7a666107b8b62f660e4643bb
차지훈 <jihun.cha@tisquare.com>
2021-10-13 19:03:34 +0900

    [동빈이의 큰 수의 법칙] - java

---
+++ src/com/huni/java/dongbins/chap01/BigNumber.java
@@ -0,0 +1,44 @@
+package com.huni.java.dongbins.chap01;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+//5 8 3
+//2 4 5 4 6
+public class BigNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int k = sc.nextInt();
+
+        System.out.println(n + " " + m + " " + k);
+
+        int[] myList = new int[n];
+        for (int i = 0; i < n; i++) {
+            myList[i] = sc.nextInt();
+        }
+
+        System.out.println(Arrays.toString(myList));
+
+        Arrays.sort(myList);
+//        Collections.sort(myList);
+
+        int max_value = myList[n-1];
+        int second_max = myList[n-2];
+
+        int result = 0;
+        for (int j = 0; j < m; j++) {
+            if ((j + 1) % k != 0) {
+                result += max_value;
+            } else {
+                result += second_max;
+            }
+        }
+
+        System.out.println(result);
+
+    }
+}
--- src/com/huni/java/dongbins/chap01/Test_01.java
+++ src/com/huni/java/dongbins/chap01/Test_01.java
@@ -1,4 +1,18 @@
 package com.huni.java.dongbins.chap01;
 
 public class Test_01 {
+    //거스름돈
+    public static void main(String[] args) {
+        int n = 1260;
+        int cnt = 0;
+        int[] coinTypes = {500, 100, 50, 10};
+
+        for (int i = 0; i < 4; i++) {
+            int coin = coinTypes[i];
+            cnt += n / coin;
+            n %= coin;
+        }
+
+        System.out.println(cnt);
+    }
 }
