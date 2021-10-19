be12135ecff911038ebfa087304a0a23f8a137be
차지훈 <>
2021-10-19 22:50:19 +0900

    임시코드

---
+++ src/com/huni/practice/Main.java
@@ -0,0 +1,27 @@
+package com.huni.practice;
+
+import java.util.*;
+
+public class Main {
+    public static Stack<Character> stack = new Stack<>();
+    public static Map<Integer, Integer> test = new HashMap<>();
+    public static void main(String[] args) {
+        String text = "1 2 3 4 5";
+
+        System.out.println(text.split(" ").length);
+
+        String[] temp = text.split(" ");
+
+        System.out.println(Integer.parseInt(temp[0]));
+
+        Test myCtest = new Test();
+
+        List test = new ArrayList();
+
+
+    }
+
+    static class Test {
+
+    }
+}
--- src/com/huni/practice/backjoon/dfsbfs/ProblemOne.java
+++ src/com/huni/practice/backjoon/dfsbfs/ProblemOne.java
@@ -4,12 +4,13 @@ package com.huni.practice.backjoon.dfsbfs;
 
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ProblemOne {
 
     public static int n,m,k,x;
-    public static ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -21,33 +22,55 @@ public class ProblemOne {
 
         sc.nextLine();
 
-        for (int i = 0; i < m; i++) {
-            graph.add(new ArrayList<Node>());
-//            String text = sc.nextLine();
-            graph.get(i).add(new Node(sc.nextInt(), sc.nextInt()));
-            sc.nextLine();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<Integer>());
         }
 
-        // 그래프 출력
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < graph.get(i).size(); j++) {
-                graph.get(i).get(j).show();
-            }
-            System.out.println();
+        for (int j = 0; j < m; j++) {
+            String text = sc.nextLine();
+
+//            System.out.println(text);
+
+            int from = (int)text.charAt(0) - '0';
+            int to = (int)text.charAt(2) - '0';
+
+//            System.out.println((int)text.charAt(0) - '0');
+
+            graph.get(from).add(to);
+//            graph.get(j).add()
         }
+
+//        System.out.println(graph.get(3).toString());
+
+
+
+//        for (int i = 0; i < m; i++) {
+//            graph.add(new ArrayList<Node>());
+////            String text = sc.nextLine();
+//            graph.get(i).add(new Node(sc.nextInt(), sc.nextInt()));
+//            sc.nextLine();
+//        }
+//
+//        // 그래프 출력
+//        for (int i = 0; i < m; i++) {
+//            for (int j = 0; j < graph.get(i).size(); j++) {
+//                graph.get(i).get(j).show();
+//            }
+//            System.out.println();
+//        }
     }
 }
-
-class Node {
-    private int index;
-    private int distance;
-
-    public Node(int index, int distance) {
-        this.index = index;
-        this.distance = distance;
-    }
-
-    public void show() {
-        System.out.print("(" + this.index + "," + this.distance + ") ");
-    }
-}
+//
+//class Node {
+//    private int index;
+//    private int distance;
+//
+//    public Node(int index, int distance) {
+//        this.index = index;
+//        this.distance = distance;
+//    }
+//
+//    public void show() {
+//        System.out.print("(" + this.index + "," + this.distance + ") ");
+//    }
+//}
